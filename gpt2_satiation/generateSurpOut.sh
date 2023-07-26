49c9e74de21a91bbdc2b2585567cf9c6764b4a3b
liankwang <lianwang0713@gmail.com>
2023-07-26 14:48:02 -0700

    SurpOut files wiht new data

--- gpt2_satiation/generateSurpOut.sh
+++ gpt2_satiation/generateSurpOut.sh
@@ -8,9 +8,9 @@
 # model10="../gpt2_satiation/checkpoints/${1}_10_0724-1epoch/checkpoint-10"
 # save10="../gpt2_satiation/SurpOut/gen_${1}10train_${2}test_SurpOut.txt"
 
-for train_cond in 'WH' 'SUBJ'
+for train_cond in 'WH' 'SUBJ' 'CNPC' 'POLAR'
 do
-    for test_cond in 'CNPC'
+    for test_cond in 'CNPC' 'WH' 'SUBJ' 'POLAR'
     do
         for num_train in 10 20 30
         do
@@ -19,7 +19,7 @@ do
             save_path="../gpt2_satiation/output/SurpOut/gen_${train_cond}${num_train}train_${test_cond}test_SurpOut.txt"
             #echo $test_data
             
-            HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 \
+            #HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 \
             python scripts/surprisal.py $test_data $model_path | tee $save_path > /dev/null
         done
     done
