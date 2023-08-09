16cb0434f75922d867cb91ada55f7b90539190e4
liankwang <lianwang0713@gmail.com>
2023-08-09 16:30:40 -0700

    SurpOut files for gen_WS

--- gpt2_satiation/generateSurpOut.sh
+++ gpt2_satiation/generateSurpOut.sh
@@ -10,11 +10,11 @@
 
 for train_cond in 'CNPC' 'POLAR' 'SUBJ' 'WH'
 do
-    for num_train in 0 #num_train is being used as num_unscrambled
+    for num_train in 0 10 20 30 #num_train is being used as num_WS
     do
         test_data="../gpt2_satiation/datasets/old/gen_${train_cond}_test15.txt"
-        model_path="../gpt2_satiation/checkpoints/${train_cond}_30_B_${num_train}_WS_0724-1epoch/checkpoint-30"
-        save_path="../gpt2_satiation/output/SurpOut/train30B_WS_test15/${train_cond}_30_B_${num_train}_WS_SurpOut.txt"
+        model_path="../gpt2_satiation/checkpoints/gen_${train_cond}_train30_${num_train}_WS_0724-1epoch/checkpoint-30"
+        save_path="../gpt2_satiation/output/SurpOut/gen_WS/gen_${train_cond}_train30_${num_train}_WS_SurpOut.txt"
         
         #HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 \
         python scripts/surprisal.py $test_data $model_path | tee $save_path > /dev/null
