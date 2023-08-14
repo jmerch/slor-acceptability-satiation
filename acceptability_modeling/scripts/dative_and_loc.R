ba443264179c2f2375e3f591d989fb6abb2858ac
jmerch <jmerchan@stanford.edu>
2023-08-14 16:08:09 -0700

    wh fix, restored lu kim

--- acceptability_modeling/scripts/dative_and_loc.R
+++ acceptability_modeling/scripts/dative_and_loc.R
@@ -3,35 +3,36 @@ library(ggplot2)
 library(ggthemes)
 library(Rmisc)
 
-dative_base = read.csv("data/lu_kim_2022/dativebaseRatings.csv")
-dative_base_surps = read.csv("data/lu_kim_2022/dativebase_surprisals.csv")
+dative_base = read.csv("acceptability_modeling/data/lu_kim_2022/dativebaseRatings.csv")
+dative_base_surps = read.csv("acceptability_modeling/data/lu_kim_2022/dativebase_surprisals.csv")
 dative_base$normalized_surprisal = dative_base_surps$normalized
-dative = read.csv("data/lu_kim_2022/dativeRatings.csv")
-dative_surps = read.csv('data/lu_kim_2022/dative_surprisals.csv')
+dative = read.csv("acceptability_modeling/data/lu_kim_2022/dativeRatings.csv")
+dative_surps = read.csv('acceptability_modeling/data/lu_kim_2022/dative_surprisals.csv')
 dative$normalized_surprisal = dative_surps$normalized
 dative_all = rbind(dative_base, dative)
-locative_base = read.csv("data/lu_kim_2022/locative_baseline_fullRatings.csv")
-locative_base_surps = read.csv("data/lu_kim_2022/locative_baseline_full_surprisals.csv")
+locative_base = read.csv("acceptability_modeling/data/lu_kim_2022/locative_baseline_fullRatings.csv")
+locative_base_surps = read.csv("acceptability_modeling/data/lu_kim_2022/locative_baseline_full_surprisals.csv")
 locative_base$normalized_surprisal = locative_base_surps$normalized
-locative_results = read.csv("data/lu_kim_2022/locativeresultsRatings.csv")
-locative_results_surps = read.csv("data/lu_kim_2022/locativeresults_surprisals.csv")
+locative_results = read.csv("acceptability_modeling/data/lu_kim_2022/locativeresultsRatings.csv")
+locative_results_surps = read.csv("acceptability_modeling/data/lu_kim_2022/locativeresults_surprisals.csv")
 locative_results$normalized_surprisal = locative_results_surps$normalized
 locative_all = rbind(locative_base, locative_results)
 
-satiation = read.csv("data/satiationRatings.csv")
+satiation = read.csv("acceptability_modeling/data/satiationRatings.csv")
 satiation = select(satiation, c("sentence_id", "condition", "all_exposures", "mean_surprisal", "normalized"))
-satitation = satiation %>%
+satiation = satiation %>%
   dplyr::rename(mean_acc = "all_exposures")
 all_experiments = rbind(locative_all, dative_all)
 all_experiments$mean_acc = ((all_experiments$mean_acc*7) - 1)/6
 all_experiments = all_experiments %>%
   dplyr::rename(normalized = "normalized_surprisal")
 all_experiments = select(all_experiments, c("sentence_id", "condition", "mean_acc", "mean_surprisal", "normalized"))
-all_experiments = rbind(all_experiments, satitation)
+all_experiments = rbind(all_experiments, satiation)
 
-disjunction = read.csv("data/disjunctionRatings.csv")
+disjunction = read.csv("acceptability_modeling/data/disjunctionRatings.csv")
 print(all_experiments[400,])
 
+all_experiments = rbind(all_experiments, disjunction)
 # Plot with Lu island data and dative,locative data
 
 # for acceptability error bars: create lists of the mean, upper, and lower of confidence intervals for each condition
@@ -353,8 +354,17 @@ plot = all_experiments %>%
   theme(axis.title = element_text())
 plot
 
+
+
 all_logistic = glm(mean_acc ~ mean_surprisal, all_experiments, family = "binomial")
 all_linear = lm(mean_acc~mean_surprisal, all_experiments)
 AIC(all_logistic)
 AIC(all_linear)
 summary(all_linear)
+
+
+cond_prediction_all = lm(mean_acc ~ mean_surprisal*condition, all_experiments)
+summary(cond_prediction_all)
+
+cond_prediction_islands = lm(mean_acc ~ mean_surprisal*condition, satiation)
+summary(cond_prediction_islands)
