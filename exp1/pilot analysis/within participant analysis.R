168b6c3b28db2258a25005d60c7ddc063931c37a
liankwang <lianwang0713@gmail.com>
2023-09-15 11:46:56 -0500

    Edited script and added plots for getting HM/HF vs satiation

--- exp1/pilot analysis/within participant analysis.R
+++ exp1/pilot analysis/within participant analysis.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 library(ggthemes)
-all = read.csv("exp1_pilot-trials_labeled_fixed.csv")
+all = read.csv("../pilot/exp1_pilot-trials_labeled_fixed.csv")
 no_prac = filter(all, block_number != "practice")
 no_prac$block_number = as.numeric(no_prac$block_number)
 
@@ -13,7 +13,7 @@ participant_3 %>%
   #geom_line(aes(group = condition, color = condition)) +
   #geom_point(aes(color = condition)) +
   ylim(0, 1) +
-  labs(title = "Paritipant 3 Ratings", 
+  labs(title = "Participant 3 Ratings", 
        x = "Block Number", 
        y = "Acceptability Rating") +
   theme_fivethirtyeight() +
@@ -27,7 +27,7 @@ participant_4 %>%
   #geom_line(aes(group = condition, color = condition)) +
   #geom_point(aes(color = condition)) +
   ylim(0, 1) +
-  labs(title = "Paritipant 4 Ratings", 
+  labs(title = "Participant 4 Ratings", 
        x = "Block Number", 
        y = "Acceptability Rating") +
   theme_fivethirtyeight() +
@@ -43,7 +43,7 @@ participant_5 %>%
   #geom_line(aes(group = condition, color = condition)) +
   #geom_point(aes(color = condition)) +
   ylim(0, 1) +
-  labs(title = "Paritipant 5 Ratings", 
+  labs(title = "Participant 5 Ratings", 
        x = "Block Number", 
        y = "Acceptability Rating") +
   theme_fivethirtyeight() +
@@ -57,7 +57,7 @@ participant_6 %>%
   #geom_line(aes(group = condition, color = condition)) +
   #geom_point(aes(color = condition)) +
   ylim(0, 1) +
-  labs(title = "Paritipant 6 Ratings", 
+  labs(title = "Participant 6 Ratings", 
        x = "Block Number", 
        y = "Acceptability Rating") +
   theme_fivethirtyeight() +
@@ -71,7 +71,7 @@ participant_7 %>%
   #geom_line(aes(group = condition, color = condition)) +
   #geom_point(aes(color = condition)) +
   ylim(0, 1) +
-  labs(title = "Paritipant 7 Ratings", 
+  labs(title = "Participant 7 Ratings", 
        x = "Block Number", 
        y = "Acceptability Rating") +
   theme_fivethirtyeight() +
@@ -89,19 +89,191 @@ no_prac %>%
   theme_fivethirtyeight() +
   theme(axis.title = element_text())
 
+# Calculates slope (satiation rate) and entropies of each condition for each participant
 conditions <- list("adj_island", "CSC", "agreement", "binding", "head_dir", "LBC", "NPI", "subcat", "subj_island")
 participants <- list(participant_3, participant_4, participant_5, participant_6, participant_7)
-within_participant_results <- data.frame(matrix(ncol = 4, nrow = 0))
+within_participant_results <- data.frame(matrix(ncol = 5, nrow = 0))
 for (participant in participants) {
   for(cond in conditions) {
     subset = filter(participant, condition == toString(cond))
     participant_num = subset$workerid[1]
     cur_slope = unname((coef(lm(rating ~ block_number, subset))[2]))
-    cur_entropy = 0
-    within_participant_results = rbind(within_participant_results, c(participant_num, cond, cur_slope, cur_entropy))
+    
+    print(participant_num)
+    print(cond)
+    meanings = as.data.frame(table(subset$meaning))
+    meanings$prob = meanings$Freq / sum(meanings$Freq)
+    forms = as.data.frame(table(subset$form))
+    forms$prob = forms$Freq / sum(forms$Freq)
+
+    cur_HM = -sum(meanings$prob * log2(meanings$prob))
+    cur_HF = -sum(forms$prob * log2(forms$prob))
+    within_participant_results = rbind(within_participant_results, c(participant_num, cond, as.numeric(cur_slope), as.numeric(cur_HM), as.numeric(cur_HF)))
   }
 }
-colnames(within_participant_results) <- c('participant', 'condition', 'slope', 'entropy')
+colnames(within_participant_results) <- c('participant', 'condition', 'slope', 'HM', 'HF')
+within_participant_results$slope = as.numeric(within_participant_results$slope)
+within_participant_results$HM = as.numeric(within_participant_results$HM)
+within_participant_results$HF = as.numeric(within_participant_results$HF)
+print(within_participant_results)
+
+# Graph slope vs. HM for individual participants
+participant_3 = filter(within_participant_results, participant==3)
+participant_3 %>%
+  ggplot(aes(x = HM, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  #geom_line(aes(group = condition, color = condition)) +
+  #ylim(0, 1) +
+  labs(title = "Participant 3", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  #scale_x_continuous(breaks = seq(0, 1, 0.1)) +
+  #scale_y_continuous(breaks = seq(0, 2,0.1)) +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HM_p3.png', path="exp1/plots/")
+
+participant_4 = filter(within_participant_results, participant==4)
+participant_4 %>%
+  ggplot(aes(x = HM, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 4", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HM_p4.png', path="exp1/plots/")
+
+participant_5 = filter(within_participant_results, participant==5)
+participant_5 %>%
+  ggplot(aes(x = HM, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 5", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HM_p5.png', path="exp1/plots/")
+
+participant_6 = filter(within_participant_results, participant==6)
+participant_6 %>%
+  ggplot(aes(x = HM, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 6", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HM_p6.png', path="exp1/plots/")
+
+participant_7 = filter(within_participant_results, participant==7)
+participant_7 %>%
+  ggplot(aes(x = HM, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 7", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HM_p7.png', path="exp1/plots/")
+
+within_participant_results %>%
+    ggplot((aes(x = HM, y= slope))) +
+    geom_point(aes(color = participant)) +
+    geom_smooth(aes(group = participant, color = participant), method = "lm", se=FALSE) +
+  #geom_line(aes(group = condition, color = condition)) +
+    #ylim(0, 1) +
+    labs(title = "All Participants", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+    theme_fivethirtyeight() +
+    theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HM_all.png', path="exp1/plots/")
+
+# Graph slope vs HF for each participant
+participant_3 = filter(within_participant_results, participant==3)
+participant_3 %>%
+  ggplot(aes(x = HF, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  #geom_line(aes(group = condition, color = condition)) +
+  #ylim(0, 1) +
+  labs(title = "Participant 3", 
+       x = "Entropy of form", 
+       y = "Satiation rate (slope)") +
+  #scale_x_continuous(breaks = seq(0, 1, 0.1)) +
+  #scale_y_continuous(breaks = seq(0, 2,0.1)) +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HF_p3.png', path="exp1/plots/")
+
+participant_4 = filter(within_participant_results, participant==4)
+participant_4 %>%
+  ggplot(aes(x = HF, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 4", 
+       x = "Entropy of form", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HF_p4.png', path="exp1/plots/")
+
+participant_5 = filter(within_participant_results, participant==5)
+participant_5 %>%
+  ggplot(aes(x = HF, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 5", 
+       x = "Entropy of form", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HF_p5.png', path="exp1/plots/")
+
+participant_6 = filter(within_participant_results, participant==6)
+participant_6 %>%
+  ggplot(aes(x = HF, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 6", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HF_p6.png', path="exp1/plots/")
+
+participant_7 = filter(within_participant_results, participant==7)
+participant_7 %>%
+  ggplot(aes(x = HF, y= slope)) +
+  geom_point(aes(color=condition)) +
+  geom_smooth(method = "lm", se=FALSE) +
+  labs(title = "Participant 7", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HF_p7.png', path="exp1/plots/")
+
+within_participant_results %>%
+  ggplot((aes(x = HF, y= slope))) +
+  geom_point(aes(color = participant)) +
+  geom_smooth(aes(group = participant, color = participant), method = "lm", se=FALSE) +
+  #geom_line(aes(group = condition, color = condition)) +
+  #ylim(0, 1) +
+  labs(title = "All Participants", 
+       x = "Entropy of meaning", 
+       y = "Satiation rate (slope)") +
+  theme_fivethirtyeight() +
+  theme(axis.title = element_text())
+ggsave(filename = 'slope_v_HF_all.png', path="exp1/plots/")
+
+
 #p7_test = filter(participant_7, condition == "LBC")
 #coef(lm(rating~block_number, p7_test))
 
